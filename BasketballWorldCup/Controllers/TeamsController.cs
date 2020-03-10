4a9dace8a7e66486b042f49190cfbbd523c74de4
Damian Ubowski <sverda300@gmail.com>
2020-03-10 20:41:47 +0100

    Return mocked teams from controller

---
+++ BasketballWorldCup/Controllers/TeamsController.cs
@@ -0,0 +1,21 @@
+﻿using BasketballWorldCup.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasketballWorldCup.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeamsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetTeams()
+        {
+            var teams = new[]
+            {
+                new Team {Id = 1, Name = "Poland"},
+                new Team {Id = 2, Name = "United Kingdoms"}
+            };
+            return Ok(teams);
+        }
+    }
+}
