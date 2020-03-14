2a563986310a36f5b17c5744985608ea8fd05f65
Damian Ubowski <sverda300@gmail.com>
2020-03-14 18:23:42 +0100

    Setup db with simple Team model

---
+++ BasketballWorldCup.Database/BasketballContext.cs
@@ -0,0 +1,16 @@
+﻿using BasketballWorldCup.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace BasketballWorldCup.Database
+{
+    public class BasketballContext : DbContext
+    {
+        public DbSet<Team> Teams { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //TODO: Move to user's secret
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=BasketballWorldCup;Trusted_Connection=True;");
+        }
+    }
+}
