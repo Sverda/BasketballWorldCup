173db912236df184a672679c13116ce6b2555069
Damian Ubowski <sverda300@gmail.com>
2020-03-22 17:26:43 +0100

    Get zones from api

--- BasketballWorldCup/ClientApp/src/app/add-team/add-team.component.ts
+++ BasketballWorldCup/ClientApp/src/app/add-team/add-team.component.ts
@@ -1,21 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TeamsService, Team } from "../services/teams.service";
+import { ZonesService } from "../services/zones.service";
 
 @Component({
     selector: 'add-team',
     templateUrl: './add-team.component.html'
 })
-export class AddTeamComponent {
-  teamsService: TeamsService;
-  newTeam: Team;
-  tiers: number[];
-  zones: string[];
+export class AddTeamComponent implements OnInit {
 
-  constructor(teamsService: TeamsService) {
+  private readonly zonesService: ZonesService;
+  private readonly teamsService: TeamsService;
+  private newTeam: Team;
+  private tiers: number[];
+  private zones: string[];
+
+  constructor(teamsService: TeamsService, zonesService: ZonesService) {
     this.teamsService = teamsService;
+    this.zonesService = zonesService;
+  }
+
+  ngOnInit(): void {
     this.newTeam = { id: 0, name: "", tier: 1, qualificationZone: "" };
     this.tiers = [0, 1, 2, 3];
-    this.zones = ["Africa", "Asia & Oceania", "Europe", "Americas"];
+    this.zonesService.getZones().subscribe(result => { this.zones = result; }, error => console.error(error));
   }
 
   addTeam() {
---
+++ BasketballWorldCup/ClientApp/src/app/services/zones.service.ts
@@ -0,0 +1,16 @@
+import { Injectable, Inject } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({ providedIn: 'root' })
+export class ZonesService {
+  baseUrl: string;
+  zonesUrl = "api/zones";
+
+  constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    this.baseUrl = baseUrl;
+  }
+
+  getZones() {
+    return this.http.get<string[]>(this.baseUrl + this.zonesUrl);
+  }
+}
