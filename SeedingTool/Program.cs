f0c68d0f3f885f7289c5842bf901b6ce023547dc
Damian Ubowski <sverda300@gmail.com>
2020-04-08 22:11:07 +0200

    Save teams to db from seeding tool

--- SeedingTool/Program.cs
+++ SeedingTool/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace SeedingTool
+﻿using BasketballWorldCup.Database;
+using BasketballWorldCup.Model;
+using Microsoft.EntityFrameworkCore;
+using SeedingTool.HtmlTeamsData.Helpers;
+
+namespace SeedingTool
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var options = new DbContextOptionsBuilder<BasketballContext>()
+                .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=BasketballWorldCup;Trusted_Connection=True;")
+                .Options;
+            using var context = new BasketballContext(options);
+            using var transaction = context.Database.BeginTransaction();
 
+            var africaMapper = new HtmlTeamMapper("./HtmlTeamsData/AfricaTeams.html", QualificationZone.Africa);
+            var africaTeams = africaMapper.MapHtmlIntoTeamsModel();
+            context.Teams.AddRange(africaTeams);
+
+            var americasMapper = new HtmlTeamMapper("./HtmlTeamsData/AmericasTeams.html", QualificationZone.Americas);
+            var americasTeams = americasMapper.MapHtmlIntoTeamsModel();
+            context.Teams.AddRange(americasTeams);
+
+            var asiaMapper = new HtmlTeamMapper("./HtmlTeamsData/AsiaTeams.html", QualificationZone.AsiaAndOceania);
+            var asiaTeams = asiaMapper.MapHtmlIntoTeamsModel();
+            context.Teams.AddRange(asiaTeams);
+
+            var europeMapper = new HtmlTeamMapper("./HtmlTeamsData/EuropeTeams.html", QualificationZone.Europe);
+            var europeTeams = europeMapper.MapHtmlIntoTeamsModel();
+            context.Teams.AddRange(europeTeams);
+
+            transaction.Commit();
         }
     }
 }
